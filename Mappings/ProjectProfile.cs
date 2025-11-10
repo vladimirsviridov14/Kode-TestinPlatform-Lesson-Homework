029cfc48bca5d6a56bea6694b9066ef94387a22f
SviridovMWP <150802249+vladimirsviridov14@users.noreply.github.com>
2025-11-10 15:54:25 +0700

    Add files via upload

---
+++ Mappings/ProjectProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TestingPlatform.Application.Dtos;
+using TestingPlatform.Respones.Group;
+using TestingPlatform.Respones.Project;
+namespace TestingPlatform.Mappings
+{
+    public class ProjectProfile: Profile
+    {
+        public ProjectProfile()
+        {
+            CreateMap<ProjectDto, ProjectRespones>();
+        }
+    }
+}
---
+++ Mappings/UserProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using TestingPlatform.Application.Dtos;
+using TestingPlatform.Models;
+using TestingPlatform.Requests.Student;
+using TestingPlatform.Respones.Student;
+
+namespace TestingPlatform.Mappings
+{
+    public class UserProfile : Profile
+    {
+
+        public UserProfile()
+        {
+             CreateMap<User, UserDto>();
+             CreateMap<UserDto, User>()
+            .ForMember(d => d.PasswordHash, o => o.MapFrom(u => u.Password));
+            
+        }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
