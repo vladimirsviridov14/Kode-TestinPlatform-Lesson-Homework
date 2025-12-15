5dd14512e4fbca142bf6d46e0441d678b1b83e16
SviridovMWP <150802249+vladimirsviridov14@users.noreply.github.com>
2025-12-15 19:53:10 +0700

    Add files via upload

---
+++ TestingPlatform/Extensions/HttpContextExtension.cs
@@ -0,0 +1,18 @@
+﻿using TestingPlatform.Constans;
+
+namespace TestingPlatform.Extensions
+{
+    public static class HttpContextExtension
+    {
+        public static int TryGetUserId(this HttpContext context)
+        {
+            var studentIdValue = context.User.Claims
+             .FirstOrDefault(c => c.Type == TestingPlatformClaimType.StudentId)?.Value;
+
+            if (!int.TryParse(studentIdValue, out var studentId))
+                throw new InvalidOperationException("User is not authorized as a student.");
+
+            return studentId;
+        }
+    }
+}
