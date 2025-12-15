50a12e03c1df5b548f881216093a218924ea6463
SviridovMWP <150802249+vladimirsviridov14@users.noreply.github.com>
2025-12-15 19:47:33 +0700

    Add files via upload

---
+++ TestingPlatform/Controllers/Attempt.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestingPlatform.Application.Dtos;
+using TestingPlatform.Application.Interfaces;
+using TestingPlatform.Constans;
+using TestingPlatform.Extensions;
+using TestingPlatform.Requests.Attempt;
+
+namespace TestingPlatform.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Attempt(IAttemptRepository attemptRepository, IMapper mapper) : ControllerBase
+
+    {
+
+        [HttpPost]
+      public async Task<IActionResult> CreateAttempt(CreateAttemptRequest attempt)
+        {
+         var studentId = HttpContext.TryGetUserId();
+
+            var attemptDto = mapper.Map<AttemptDto>(attempt);
+            attemptDto.StudentId = studentId;
+
+
+            var attemptId = await attemptRepository.CreateAsync(attemptDto);
+           
+            return StatusCode(StatusCodes.Status201Created, new { Id = attemptId});
+        }
+
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateAttemptAsync(CreateAttemptRequest attempt)
+        {
+            var studentId = HttpContext.TryGetUserId();
+
+            var attemptDto = mapper.Map<AttemptDto>(attempt);
+            attemptDto.StudentId = studentId;
+
+            await attemptRepository.UpdateAsync(attemptDto);
+            return StatusCode(StatusCodes.Status200OK);
+        }
+    }
+}
---
+++ TestingPlatform/Controllers/TestResults.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestingPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestResults : ControllerBase
+    {
+    }
+}
