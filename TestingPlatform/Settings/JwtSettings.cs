15391fec3c6dbc5d20af041bd3d81398529e3777
SviridovMWP <150802249+vladimirsviridov14@users.noreply.github.com>
2025-12-15 19:59:38 +0700

    Add files via upload

---
+++ TestingPlatform/Settings/JwtSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace TestingPlatform.Settings
+{
+    public class JwtSettings
+    {
+        public string Key { get; set; }
+
+        public string Issuer { get; set; }
+
+        public string Audience { get; set; }
+
+        public int AccessTokenMinutes { get; set; }
+
+        public int RefreshTokenDays { get; set; }
+
+    }
+}
